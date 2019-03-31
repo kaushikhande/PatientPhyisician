195edccb3883ae129dcbaf765d81c0dba2030281
kaushikhande <kaushikhande302@gmail.com>
2019-03-31 16:07:51 +0530

    devise-gem-added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get 'welcome/home'
+  get 'welcome/about'
+  get 'welcome/contact'
   get 'physicians/index'
   get 'patients/index'
+
+  root 'welcome#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
